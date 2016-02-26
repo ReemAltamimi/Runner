a166f3c8b89832474d9b619b014a72933c20dc0d
RAISTLIN\Ben <Readd scripts>
2016-02-26 13:38:59 +1100

    * End level trigger * Added a stub second level.

---
+++ Assets/EndLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+    public string nextLevel = "Level2";
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("entered the trigger");
+        if (other.gameObject.tag == "Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
