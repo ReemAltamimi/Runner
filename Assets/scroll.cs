caec10ae681e1dcafffd270bc93a908302966942
ReemAltamimi <reem.altamimi@uon.edu.au>
2016-04-01 10:01:30 +1100

    new levels

---
+++ Assets/scroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+    public float speed = 0;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+
+
+    }
+}
