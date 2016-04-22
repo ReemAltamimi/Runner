19b4830201975bf1d81586120e050efd107d2005
RAISTLIN\Ben <Readd scripts>
2016-04-22 13:53:17 +1000

    DotNetOpenAuth Client - NOT WORKING

--- MyStepsSite/StepsWeb/StepsWeb/PhysicalActivityManagement.aspx.cs
+++ MyStepsSite/StepsWeb/StepsWeb/PhysicalActivityManagement.aspx.cs
@@ -6,46 +6,38 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Net;
 using System.Xml;
-using OAuth.Net.Common;
-using OAuth.Net.Components;
-using OAuth.Net.Consumer;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Text.RegularExpressions;
-using EndPoint = OAuth.Net.Consumer.EndPoint;
+using DotNetOpenAuth.OAuth2;
+using System.Net.Http;
 
 namespace MySteps
 {
     public partial class PhysicalActivityManagement : System.Web.UI.Page
     {
-        string userId;
-        string distanceAsString;
-        string distanceAsNumber;
-        double distance;
+        
         protected XmlDocument Doc { get; private set; }
-
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            userId = Session["UserId"].ToString();
+            //userId = Session["UserId"].ToString();
             Session["DateTime"] = DateTime.Now;
 
-            const string ConsumerKey = "aca55fdfdb7d48f3a0c3f5c6505e5567";
+            const string FitbitClientId = "227L5Z";
             //"local-fitbit-example-client-application";
             //"aca55fdfdb7d48f3a0c3f5c6505e5567"
-            const string ConsumerSecret = "8e97224b8df64178a2970559604fafeb";
+            const string FitbitSecret = "26fed8d066abe1bb28e98260f7d4578c";
             //"your.secret.key.from.dev.fitbit.com";
             //"8e97224b8df64178a2970559604fafeb"
 
             // API call path to get temporary credentials (request token and secret)
-            const string RequestTokenUrl = "https://api.fitbit.com/oauth/request_token";
+            Uri fitBitRequestTokenUrl = new Uri("https://api.fitbit.com/oauth2/token");
 
             // Base path of URL where the user will authorize this application
-            const string AuthorizationUrl = "https://www.fitbit.com/oauth/authorize";
+            Uri authorizationUri = new Uri("https://www.fitbit.com/oauth2/authorize");
 
-            // API call path to get token credentials (access token and secret)
-            const string AccessTokenUrl = "https://api.fitbit.com/oauth/access_token";
-
-            // API call path to protected resource
+                        // API call path to protected resource
             DateTime dt = DateTime.Now;
             string sDay = Convert.ToString(dt.Day);
             string sMonth = Convert.ToString(dt.Month);
@@ -59,72 +51,85 @@ namespace MySteps
 
             /*const string ApiCallUrl = "http://api.fitbit.com/1/user/-/activities/date/2014-05-17.xml";*/
 
-            // Create OAuthService object, containing oauth consumer configuration
-            OAuthService service = OAuthService.Create(
-                new EndPoint(RequestTokenUrl, "POST"),         // requestTokenEndPoint
-                new Uri(AuthorizationUrl),                     // authorizationUri
-                new EndPoint(AccessTokenUrl, "POST"),          // accessTokenEndPoint
-                true,                                          // useAuthorizationHeader
-                "http://api.fitbit.com",                       // realm
-                "HMAC-SHA1",                                   // signatureMethod
-                "2.0",                                         // oauthVersion
-                new OAuthConsumer(ConsumerKey, ConsumerSecret) // consumer
-                );
+            //// Create OAuthService object, containing oauth consumer configuration
+            //OAuthService service = OAuthService.Create(
+            //    new EndPoint(RequestTokenUrl, "POST"),         // requestTokenEndPoint
+            //    new Uri(AuthorizationUrl),                     // authorizationUri
+            //    new EndPoint(AccessTokenUrl, "POST"),          // accessTokenEndPoint
+            //    true,                                          // useAuthorizationHeader
+            //    "http://api.fitbit.com",                       // realm
+            //    "HMAC-SHA1",                                   // signatureMethod
+            //    "2.0",                                         // oauthVersion
+            //    new OAuthConsumer(FitbitClientId, ConsumerSecret) // consumer
+            //    );
+
+            var authorizationServer = new AuthorizationServerDescription
+            {
+                AuthorizationEndpoint = authorizationUri,
+                TokenEndpoint = fitBitRequestTokenUrl
+
+            };
 
             try
             {
-                // Create OAuthRequest object, providing protected resource URL, consumer configuration,
-                // callback URL and current session identifier
-                OAuthRequest request = OAuthRequest.Create(
-                    new EndPoint(ApiCallUrl, "GET"),
-                    service,
-                    this.Context.Request.Url,
-                    this.Context.Session.SessionID);
+                WebServerClient client = new WebServerClient(authorizationServer, FitbitClientId, FitbitSecret);
+                var token = Session["access_token"];
 
-                // Assign verification handler delegate
-                request.VerificationHandler = AspNetOAuthRequest.HandleVerification;
 
-                // Call OAuthRequest object GetResource method, which returns OAuthResponse object
-                OAuthResponse response = request.GetResource();
-
-                // Check if OAuthResponse object has protected resource
-                if (!response.HasProtectedResource)
+                if (Session["access_token"] == null)
                 {
-                    // If not we are not authorized yet, build authorization URL and redirect to it
-                    string authorizationUrl = service.BuildAuthorizationUrl(response.Token).AbsoluteUri;
-                    Response.Redirect(authorizationUrl);
+                    var userAuthorization = client.PrepareRequestUserAuthorization(new[] { "activity" });
+                    userAuthorization.Send(Context);
+                    Session["requested"] = true;
+                    Response.End();
                 }
 
-                // Store the access token in session variable
-                Session["access_token"] = response.Token;
+                else if ((bool)(Session["requested"]))
+                {
+                    var authorizationState = client.ProcessUserAuthorization();
+                    if (authorizationState != null)
+                    {
+                        Session["access_token"] = authorizationState.AccessToken;
+                        Session["refresh_token"] = authorizationState.RefreshToken;
+                    }
+                }
 
-                // Initialize the XmlDocument object and OAuthResponse object's protected resource to it
-                this.Doc = new XmlDocument();
-                this.Doc.Load(response.ProtectedResource.GetResponseStream());
 
-                //get the distance value from the xml document
-                distanceAsString = this.Doc["result"]["summary"]["distances"].FirstChild.InnerText; //this will retrieve"total+distanceAsNumber"
-                distanceAsNumber = Regex.Match(distanceAsString, @"\d+(\.\d+)").Value.Replace(" ", ""); //this will extract the distanceAsNumber only
-                if (!double.TryParse(distanceAsNumber, out distance))
-                    distance = 0.0;
-
-                //Insert data to database
-                //add Physical activity data into PhysicalActivityData table
-                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegisterationConnectionString"].ConnectionString);
-                conn.Open();
-                string insertQuery = "insert into PhysicalActivityData (UserID,DateAndTime,DailySteps,DistanceWalkedMile,EstimatedActiveTime) values (@id, @dateTime, @steps, @distance, @activetime)";
-                SqlCommand command = new SqlCommand(insertQuery, conn);
+                //var authorizationState = client.ProcessUserAuthorization();
+                //if (authorizationState != null)
+                //{
+                //    var userAuthorization = client.PrepareRequestUserAuthorization(new[] { "activity" });
+                //    userAuthorization.Send(Context);
+                //    Response.End();
+                //}
 
                 
-                command.Parameters.AddWithValue("@id", Convert.ToInt32(userId));
-                command.Parameters.AddWithValue("@dateTime", Session["DateTime"]);
-                command.Parameters.AddWithValue("@steps", Convert.ToInt32(this.Doc["result"]["summary"]["steps"].InnerText));
-                command.Parameters.AddWithValue("@distance", distance);
-                command.Parameters.AddWithValue("@activetime", Convert.ToInt32(this.Doc["result"]["summary"]["veryActiveMinutes"].InnerText));
 
-                command.ExecuteNonQuery();
+               
 
-                conn.Close();
+                ////get the distance value from the xml document
+                //distanceAsString = this.Doc["result"]["summary"]["distances"].FirstChild.InnerText; //this will retrieve"total+distanceAsNumber"
+                //distanceAsNumber = Regex.Match(distanceAsString, @"\d+(\.\d+)").Value.Replace(" ", ""); //this will extract the distanceAsNumber only
+                //if (!double.TryParse(distanceAsNumber, out distance))
+                //    distance = 0.0;
+
+                ////Insert data to database
+                ////add Physical activity data into PhysicalActivityData table
+                //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegisterationConnectionString"].ConnectionString);
+                //conn.Open();
+                //string insertQuery = "insert into PhysicalActivityData (UserID,DateAndTime,DailySteps,DistanceWalkedMile,EstimatedActiveTime) values (@id, @dateTime, @steps, @distance, @activetime)";
+                //SqlCommand command = new SqlCommand(insertQuery, conn);
+
+                
+                //command.Parameters.AddWithValue("@id", Convert.ToInt32(userId));
+                //command.Parameters.AddWithValue("@dateTime", Session["DateTime"]);
+                //command.Parameters.AddWithValue("@steps", Convert.ToInt32(this.Doc["result"]["summary"]["steps"].InnerText));
+                //command.Parameters.AddWithValue("@distance", distance);
+                //command.Parameters.AddWithValue("@activetime", Convert.ToInt32(this.Doc["result"]["summary"]["veryActiveMinutes"].InnerText));
+
+                //command.ExecuteNonQuery();
+
+                //conn.Close();
                
             }
             catch (WebException ex)
@@ -132,7 +137,7 @@ namespace MySteps
                 Response.Write(ex.Message);
                 Response.Close();
             }
-            catch (OAuthRequestException ex)
+            catch (Exception ex)
             {
                 Response.Write(ex.Message);
                 Response.Close();
