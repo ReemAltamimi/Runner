2022b12067d8a98c52ac206a530eedf38b886fa1
ReemAltamimi <reem.altamimi@uon.edu.au>
2016-05-10 22:45:20 +1000

    get physical data from fitbit, save all in database, print results on physical activity management page and mapping the steps to the game speed in game page

--- MySteps/Game/Game.aspx.cs
+++ MySteps/Game/Game.aspx.cs
@@ -7,10 +7,31 @@ using System.Web.UI.WebControls;
 
 public partial class Default2 : System.Web.UI.Page
 {
-    
+    string userId;
+    int steps;
 
-    public String GetMovement(){
-        int steps = 10000; // Session[Steps]
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //check if the user is login in the system
+        if (Session["UserId"] == null)
+        {
+            Response.Redirect("~/Default.aspx");
+            return;
+        }
+
+        userId = Session["UserId"].ToString();
+        Session["DateTime"] = DateTime.Now;
+        steps = Convert.ToInt32(Session["Steps"]);
+
+        //get the mode of the game
+        String mode = GetMovement();
+    }
+
+
+    public String GetMovement()
+    {
+        //int steps = 10000; // Session[Steps]
         if (steps >= 10000)
         {
             return "Fast";
@@ -23,8 +44,5 @@ public partial class Default2 : System.Web.UI.Page
         return "Slow";
     }
 
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
+    
 }
--- MySteps/PhysicalActivityManagement.aspx.cs
+++ MySteps/PhysicalActivityManagement.aspx.cs
@@ -22,9 +22,10 @@ namespace MySteps
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            string token, refreshToken, id;
+            //variables declaration
+            string token, refreshToken;
 
-            //check the user id
+            //check if the user is login in the system
             if (Session["UserId"] == null)
                 {
                    Response.Redirect("Default");
@@ -49,15 +50,15 @@ namespace MySteps
             Uri authorizationUri = new Uri("https://www.fitbit.com/oauth2/authorize");
 
             // API call path to protected resource
-            DateTime dt = DateTime.Now;
-            string sDay = Convert.ToString(dt.Day);
-            string sMonth = Convert.ToString(dt.Month);
-            string sYear = Convert.ToString(dt.Year);
+            //DateTime dt = DateTime.Now;
+            //string sDay = Convert.ToString(dt.Day);
+            //string sMonth = Convert.ToString(dt.Month);
+            //string sYear = Convert.ToString(dt.Year);
             
 
-            string strFitBit = "http://api.fitbit.com/1/user/-/activities/date/" + sYear + "-" + sMonth + "-" + sDay + ".xml";
+            //string strFitBit = "http://api.fitbit.com/1/user/-/activities/date/" + sYear + "-" + sMonth + "-" + sDay + ".xml";
 
-            string ApiCallUrl = strFitBit;
+            //string ApiCallUrl = strFitBit;
 
 
             
@@ -78,36 +79,21 @@ namespace MySteps
                 {
                     var userAuthorization = client.PrepareRequestUserAuthorization(new[] { "activity" });
                     userAuthorization.Send(Context);
-                    //id = userAuthorization.Body.ToString();
-
                 }
                 else
                 {
                     Session["access_token"] = authorizationState.AccessToken;
                     Session["refresh_token"] = authorizationState.RefreshToken;
-                   // id = userAuthorization.Body.ToString();
                 }
 
                 token = authorizationState.AccessToken.ToString();
                 refreshToken = authorizationState.RefreshToken.ToString();
-                //id =;
-                //var authorizationState = client.ProcessUserAuthorization();
-                //if (authorizationState != null)
-                //{
-                //    var userAuthorization = client.PrepareRequestUserAuthorization(new[] { "activity" });
-                //    userAuthorization.Send(Context);
-                //    Response.End();
-                //}
-
-                //============================================================================
-
-
-
+    
                 if (!IsPostBack)
                 {
                     try
                     {
-                        //add user data entered in registeration page into UserData table
+                        //add token and refresh token into UserData table
                         SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegisterationConnectionString"].ConnectionString);
                         conn.Open();
                         string insertQuery = "UPDATE UserData SET Token = @token, RefreshToken = @refreshToken WHERE(Id = @userId)";
@@ -124,34 +110,6 @@ namespace MySteps
                         Label2.Text = "Error :" + ex.ToString();
                     }
                 }
-                
-
-
-
-                ////get the distance value from the xml document
-                //distanceAsString = this.Doc["result"]["summary"]["distances"].FirstChild.InnerText; //this will retrieve"total+distanceAsNumber"
-                //distanceAsNumber = Regex.Match(distanceAsString, @"\d+(\.\d+)").Value.Replace(" ", ""); //this will extract the distanceAsNumber only
-                //if (!double.TryParse(distanceAsNumber, out distance))
-                //    distance = 0.0;
-
-                ////Insert data to database
-                ////add Physical activity data into PhysicalActivityData table
-                //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegisterationConnectionString"].ConnectionString);
-                //conn.Open();
-                //string insertQuery = "insert into PhysicalActivityData (UserID,DateAndTime,DailySteps,DistanceWalkedMile,EstimatedActiveTime) values (@id, @dateTime, @steps, @distance, @activetime)";
-                //SqlCommand command = new SqlCommand(insertQuery, conn);
-
-
-                //command.Parameters.AddWithValue("@id", Convert.ToInt32(userId));
-                //command.Parameters.AddWithValue("@dateTime", Session["DateTime"]);
-                //command.Parameters.AddWithValue("@steps", Convert.ToInt32(this.Doc["result"]["summary"]["steps"].InnerText));
-                //command.Parameters.AddWithValue("@distance", distance);
-                //command.Parameters.AddWithValue("@activetime", Convert.ToInt32(this.Doc["result"]["summary"]["veryActiveMinutes"].InnerText));
-
-                //command.ExecuteNonQuery();
-
-                //conn.Close();
-
             }
             catch (WebException ex)
             {
@@ -160,37 +118,122 @@ namespace MySteps
             }
 
 
-            //===============================================================================
+
             //Request the user workout.
+
+            //Get the date of today in a "yyyy-MM-dd" format
             String activityDate = DateTime.Now.ToString("yyyy-MM-dd");
 
-            string urlworkout = "https://api.fitbit.com/1/user/" + "-" + "/activities/steps/date/" + activityDate + "/1d.json";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlworkout);
-            request.Method = "GET";
-            request.Headers["Authorization"] = "Bearer " + Session["access_token"];
-            request.Accept = "application/json";
+            //create string of 2D array to hold the http requests and responses.
+            string[,] workouts = new string[6, 2]{
+                { "https://api.fitbit.com/1/user/" + "-" + "/activities/steps/date/" + activityDate + "/1d.json", "" },
+                { "https://api.fitbit.com/1/user/" + "-" + "/activities/distance/date/" + activityDate + "/1d.json", "" },
+                {"https://api.fitbit.com/1/user/" + "-" + "/activities/minutesSedentary/date/" + activityDate + "/1d.json", "" },
+                {"https://api.fitbit.com/1/user/" + "-" + "/activities/minutesLightlyActive/date/" + activityDate + "/1d.json", ""},
+                {"https://api.fitbit.com/1/user/" + "-" + "/activities/minutesFairlyActive/date/" + activityDate + "/1d.json", ""},
+                {"https://api.fitbit.com/1/user/" + "-" + "/activities/minutesVeryActive/date/" + activityDate + "/1d.json", ""}
+            };
 
             WebResponse myResponse;
-            string results = "";
-          
+            int steps, minSed, minLActive, minFActive, minVActive;
+            Double distance;
 
-            myResponse = request.GetResponse();
-            StreamReader httpwebStreamReader = new StreamReader(myResponse.GetResponseStream());
-            results = httpwebStreamReader.ReadToEnd();
+            //fill the array with responses
+            for (int row = 0; row < workouts.GetLength(0); row++)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(workouts[row, 0]);
+                request.Method = "GET";
+                request.Headers["Authorization"] = "Bearer " + Session["access_token"];
+                request.Accept = "application/json";
+                myResponse = request.GetResponse();
+                StreamReader httpwebStreamReader = new StreamReader(myResponse.GetResponseStream());
+                workouts[row, 1] = httpwebStreamReader.ReadToEnd();
+                myResponse.Close();
+                httpwebStreamReader.Close();
+            }
+
+            
+
+            // Assign the workouts to their values
+            steps = Convert.ToInt32(getNumberFromString(workouts[0, 1]));
+            distance = Convert.ToDouble(getNumberFromString(workouts[1, 1]));
+            minSed = Convert.ToInt32(getNumberFromString(workouts[2, 1]));
+            minLActive = Convert.ToInt32(getNumberFromString(workouts[3, 1]));
+            minFActive = Convert.ToInt32(getNumberFromString(workouts[4, 1]));
+            minVActive = Convert.ToInt32(getNumberFromString(workouts[5, 1]));
+
+            //assign number of steps to session
+            Session["Steps"] = steps;
 
 
+            // Insert data to database
+            //add Physical activity data into PhysicalActivityData table
+            //Check if the user has already insert his/her data for today
+            SqlConnection conn1 = new SqlConnection(ConfigurationManager.ConnectionStrings["RegisterationConnectionString"].ConnectionString);
+            conn1.Open();
 
-            // Initialize the XmlDocument object and OAuthResponse object's protected resource to it
-            // this.Doc = new XmlDocument();
-            //this.Doc.Load(httpwebStreamReader);
-            Label2.Text = results;
-            myResponse.Close();
-            httpwebStreamReader.Close();
+            string checkUserId = "select count(*) from PhysicalActivityData where UserID= '" + userId + "' AND DateAndTime>= DATEADD(dd, 0, DATEDIFF(dd, 0, GETDATE())) AND DateAndTime < DATEADD(dd, 1, DATEDIFF(dd, 0, GETDATE()))";
+            SqlCommand command1 = new SqlCommand(checkUserId, conn1);
+            int temp = Convert.ToInt32(command1.ExecuteScalar().ToString());
+            if (temp == 1)
+            {
+                    string updateQuery = "UPDATE PhysicalActivityData SET DateAndTime = @dateTime, DailySteps = @steps, DistanceWalkedMile = @distance, SedentaryMinutes = @sedMin, LightActiveMinutes = @lightMin, FairlyActiveMinutes = @fairlyMin, VeryActiveMinutes = @veryMin  WHERE UserID = '" + userId + "' AND DateAndTime>= DATEADD(dd, 0, DATEDIFF(dd, 0, GETDATE())) AND DateAndTime < DATEADD(dd, 1, DATEDIFF(dd, 0, GETDATE()))";
 
-            //===============================================================================
+                    SqlCommand command3 = new SqlCommand(updateQuery, conn1);
+
+                    command3.Parameters.AddWithValue("@dateTime", Session["DateTime"]);
+                    command3.Parameters.AddWithValue("@steps", steps);
+                    command3.Parameters.AddWithValue("@distance", distance);
+                    command3.Parameters.AddWithValue("@sedMin", minSed);
+                    command3.Parameters.AddWithValue("@lightMin", minLActive);
+                    command3.Parameters.AddWithValue("@fairlyMin", minFActive);
+                    command3.Parameters.AddWithValue("@veryMin", minVActive);
+                    command3.ExecuteNonQuery();
+             }
+             else
+             {
+                    string insertQuery1 = "insert into PhysicalActivityData (UserID,DateAndTime,DailySteps,DistanceWalkedMile,SedentaryMinutes,LightActiveMinutes,FairlyActiveMinutes,VeryActiveMinutes) values (@id, @dateTime, @steps, @distance, @sedMin, @lightMin, @fairlyMin, @veryMin)";
+                    SqlCommand command4 = new SqlCommand(insertQuery1, conn1);
+
+
+                    command4.Parameters.AddWithValue("@id", Convert.ToInt32(userId));
+                    command4.Parameters.AddWithValue("@dateTime", Session["DateTime"]);
+                    command4.Parameters.AddWithValue("@steps", steps);
+                    command4.Parameters.AddWithValue("@distance", distance);
+                    command4.Parameters.AddWithValue("@sedMin", minSed);
+                    command4.Parameters.AddWithValue("@lightMin", minLActive);
+                    command4.Parameters.AddWithValue("@fairlyMin", minFActive);
+                    command4.Parameters.AddWithValue("@veryMin", minVActive);
+
+                    command4.ExecuteNonQuery();
+                }
+
+                conn1.Close();
+
+            //print the fitbit results on screen
+            Label2.Text = "<br />";
+            Label2.Text += "Number of Steps = " + steps + "<br />";
+            Label2.Text += "The distance you have walked = " + distance + "<br />";
+            Label2.Text += "Sedentary time in minutes = " + minSed + "<br />";
+            Label2.Text += "Lightly Active Minutes= " + minLActive + "<br />";
+            Label2.Text += "Fairly Active Minutes = " + minFActive + "<br />";
+            Label2.Text += "Very Active Minutes = " + minVActive + "<br />";
 
         }
+        
+        //This Function extraxt the value numbers from a string
+        //This function is used to extract the workout values from fitbit responses
+        private string getNumberFromString(string str)
+        {
+            string strNumber;
+            
+            str = Regex.Replace(str, @"\d{4}-\d{2}-\d{2}",String.Empty);
+            strNumber = Regex.Match(str, @"\d+").Value;
+            return strNumber;
+        }
+
+
+
         protected void btnViewChart_Click(object sender, EventArgs e)
         {
             double startDate = DateTime.Now.AddDays(-1).ToOADate();
