d2bc49866d41a55ef85ffbfcdb35ef9371c83289
RAISTLIN\Ben <Readd scripts>
2016-05-13 10:05:50 +1000

    Moving platform update Fix for stuck enemies

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public float normalizedTime = 0;
+    public float speed = 1;
+	// Use this for initialization
+	void Start () {
+        var animator = GetComponent<Animator>();
+        animator.speed = speed;
+        animator.Play("platformsMoving", 0, normalizedTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
