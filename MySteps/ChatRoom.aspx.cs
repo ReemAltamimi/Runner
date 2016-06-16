82b71612f6548a2b63d6b88a6524448522e5a9a6
ReemAltamimi <reem.altamimi@uon.edu.au>
2016-06-16 12:30:58 +1000

    Design, clean and test some pages.

--- MySteps/ChatRoom.aspx.cs
+++ MySteps/ChatRoom.aspx.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 public partial class ChatRoom : System.Web.UI.Page
 {
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        //changer the header div background
+        ((HtmlGenericControl)this.Page.Master.FindControl("header")).Style.Add("background", "#99CCFF");
+
+
+        //check if the user is login in the system
         if (Session["New"] == null)
             Response.Redirect("~/LoginPage.aspx");
         else
--- MySteps/Leaderboard.aspx.cs
+++ MySteps/Leaderboard.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Web.UI.HtmlControls;
 
 public partial class Leaderboard : System.Web.UI.Page
 {
@@ -14,6 +15,9 @@ public partial class Leaderboard : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        //changer the header div background
+        ((HtmlGenericControl)this.Page.Master.FindControl("header")).Style.Add("background", "#80FF80");
+
         //check if the user is login in the system
         if (Session["UserId"] == null)
         {
@@ -25,10 +29,7 @@ public partial class Leaderboard : System.Web.UI.Page
         Session["DateTime"] = DateTime.Now;
         Session["today"] = DateTime.Today.Date;
 
-
-
-       // string str = "SELECT * From PhysicalActivityData where DateAndTime>= DATEADD(dd, 0, DATEDIFF(dd, 0, GETDATE())) AND DateAndTime < DATEADD(dd, 1, DATEDIFF(dd, 0, GETDATE()))";
-
+        //fill the list view with all users'names and their steps of today
         string str = "SELECT DailySteps, UserData.UserName From PhysicalActivityData INNER JOIN UserData ON PhysicalActivityData.UserID = UserData.Id where DateAndTime>= DATEADD(dd, 0, DATEDIFF(dd, 0, GETDATE())) AND DateAndTime < DATEADD(dd, 1, DATEDIFF(dd, 0, GETDATE()))";
 
         SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegisterationConnectionString"].ConnectionString);
@@ -36,11 +37,7 @@ public partial class Leaderboard : System.Web.UI.Page
         DataTable dt = new DataTable();
         sda.Fill(dt);
         ListView1.DataSource = dt;
-        ListView1.DataBind();
-
-
-
-        
+        ListView1.DataBind();      
 
     }
 }
