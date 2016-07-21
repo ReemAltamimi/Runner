e7d397832c3bee5bdc19e64beedf9faf353ed8a6
ReemAltamimi <reem.altamimi@uon.edu.au>
2016-07-22 01:30:16 +1000

    Create admin page

---
+++ MySteps/Admin.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Admin.aspx.cs" Inherits="Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .gridview {
+    width: 100%; 
+    word-wrap:break-word;
+    table-layout: fixed;
+}
+    </style>
+</head>
+<body style="background-color:peachpuff">
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="btnUserData" runat="server" Text="Show User Data Table" OnClick="btnUserData_Click" />
+    
+        <asp:Button ID="btnScrnTimData" runat="server" Text="Show Screen Time Table" OnClick="btnScrnTimData_Click" />
+    
+        <asp:Button ID="btnPhysicalData" runat="server" Text="Show Physical Activity Table" OnClick="btnPhysicalData_Click" />
+        <asp:Button ID="btnGameData" runat="server" Text="Show Game Data Table" OnClick="btnGameData_Click" />
+        <asp:Button ID="btnUserLevels" runat="server" Text="Show User Levels Table" OnClick="btnUserLevels_Click" />
+    
+        <asp:Button ID="btnUserPhysicalScreen" runat="server" Text="Show User, Physical & Screen Data" OnClick="btnUserPhysicalScreen_Click" />
+        <asp:Button ID="btnUserGameLevels" runat="server" Text="Show User, Game & Levels Data" OnClick="btnUserGameLevels_Click" />
+    
+        <asp:Button ID="btnForum" runat="server" Text="Show Forum Data" OnClick="btnForum_Click" />
+        <asp:Button ID="btnThread" runat="server" Text="Show Thread Data" OnClick="btnThread_Click" />
+    
+        <asp:Button ID="btnReminder" runat="server" Text="Send a Reminder" OnClick="btnReminder_Click" />
+    
+        <asp:TextBox ID="txbUserId" runat="server"></asp:TextBox>
+    
+    </div>
+    
+        <asp:Label ID="lblError" runat="server"></asp:Label>
+    
+        <asp:GridView ID="GridView1" runat="server"
+            BorderStyle="Outset" RowStyle-Wrap="true" 
+            RowStyle-HorizontalAlign="Center" RowStyle-VerticalAlign="Middle"
+            class="gridview" BorderColor="#333333" BorderWidth="2"
+            Font-Name="cambria"
+                Font-Size="10pt" Cellpadding="4"
+            HeaderStyle-BackColor="#444444"
+            HeaderStyle-ForeColor="White"
+                AlternatingRowStyle-BackColor="#dddddd">
+<RowStyle HorizontalAlign="Center" VerticalAlign="Middle" Wrap="True" Width="20px"></RowStyle>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ MySteps/Admin.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+
+public partial class Admin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnUserData_Click(object sender, EventArgs e)
+    {
+        String query = "select * from UserData";
+        GetTableData(query);
+    }
+
+    protected void btnScrnTimData_Click(object sender, EventArgs e)
+    {
+        String query = "select * from ScreenTimeData";
+        GetTableData(query);
+
+    }
+
+    protected void btnPhysicalData_Click(object sender, EventArgs e)
+    {
+        String query = "select * from PhysicalActivityData";
+        GetTableData(query);
+    }
+
+    protected void btnGameData_Click(object sender, EventArgs e)
+    {
+        String query = "select * from GameData";
+        GetTableData(query);
+    }
+
+    protected void btnUserLevels_Click(object sender, EventArgs e)
+    {
+        String query = "select * from UserLevels";
+        GetTableData(query);
+    }
+
+    protected void btnUserPhysicalScreen_Click(object sender, EventArgs e)
+    {
+        if(txbUserId.Text.Equals(""))
+        {
+            lblError.Text = "Enter User Id";
+        }
+        else
+        {
+            String query = "SELECT UserName, Id, DateAndTime, DailySteps, '' as Date, '' as UserScreenDailyAmnt from UserData, PhysicalActivityData"
+          + " where UserData.Id = PhysicalActivityData.UserID AND Id = '" + Convert.ToInt32(txbUserId.Text) + "'"
+          + " UNION ALL "
+          + "SELECT UserName, Id, '' as DateAndTime, '' as DailySteps, Date, UserScreenDailyAmnt from UserData, ScreenTimeData"
+          + " where UserData.Id = ScreenTimeData.UserID AND Id = '" + Convert.ToInt32(txbUserId.Text) + "'";
+            GetTableData(query);
+        }
+      
+    }
+
+    protected void btnUserGameLevels_Click(object sender, EventArgs e)
+    {
+        if (txbUserId.Text.Equals(""))
+        {
+            lblError.Text = "Enter User Id";
+        }
+        else
+        {
+            String query = "SELECT UserName, Id, Date, UnLockedLevel, TimeOfPlay, '' as Date, '' as Level, '' as Stars, '' as Hearts from UserData, GameData"
+          + " where UserData.Id = GameData.UserID AND Id = '" + Convert.ToInt32(txbUserId.Text) + "'"
+          + " UNION ALL "
+          + " SELECT UserName, Id, '' as Date, '' as UnLockedLevel, '' as TimeOfPlay, Date, Level, Stars, Hearts from UserData, UserLevels"
+          + " where UserData.Id = UserLevels.UserId AND Id = '" + Convert.ToInt32(txbUserId.Text) + "'";
+            GetTableData(query);
+        }
+    }
+
+    protected void btnForum_Click(object sender, EventArgs e)
+    {
+        String query = "select * from Forum";
+        GetTableData(query);
+    }
+
+    protected void btnThread_Click(object sender, EventArgs e)
+    {
+        String query = "select * from Thread";
+        GetTableData(query);
+    }
+
+    public void GetTableData(string query)
+    {
+        using (SqlConnection connection = ConnectionManager.GetDatabaseConnection())
+        {
+            
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            SqlDataReader dr;
+
+            dr = cmd.ExecuteReader();
+
+            GridView1.DataSource = dr;
+
+            GridView1.DataBind();
+
+            connection.Close();
+        }
+    }
+
+
+
+    protected void btnReminder_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            // Gmail Address from where you send the mail
+            var fromAddress = "mystepsnewcastle@gmail.com";
+            // any address where the email will be sending
+            var toAddress = "c3107877@uon.edu.au, " + getUsersEmails();
+            //Password of your gmail address
+            const string fromPassword = "Reem123456";
+            // Passing the values and make a email formate to display
+            string subject = "Reminder";
+            string body = "Hi, Have you visit Mysteps website today";
+            // smtp settings
+            var smtp = new System.Net.Mail.SmtpClient();
+            {
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+                smtp.Timeout = 20000;
+            }
+            // Passing values to smtp object
+            smtp.Send(fromAddress, toAddress, subject, body);
+            lblError.Text = "Reminders were sent";
+        }
+        catch(Exception)
+        {
+            lblError.Text = "Error Caught";
+        }
+       
+    }
+
+    public string getUsersEmails()
+    {
+        string emailsList = "";
+        try
+        {
+            //System.Data.SqlClient.SqlDataReader dr = null;
+            using (SqlConnection connection = ConnectionManager.GetDatabaseConnection())
+            {
+                SqlCommand cmd = new SqlCommand("SELECT [Email] FROM [UserData]", connection);
+
+                using (IDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (!String.IsNullOrEmpty(emailsList))
+                        {
+                            emailsList += ", ";
+                        }
+                        emailsList += reader["Email"].ToString();
+                    }
+                }
+                connection.Close();       
+
+            }
+        }
+        catch (Exception)
+        {
+            lblError.Text = "Error Caught";
+        }
+
+        return emailsList;
+    }
+}
